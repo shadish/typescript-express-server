30db1c7fae003d1710545c7181b4d4a6d3603719
shadish <shadtisch@gmail.com>
2018-11-05 21:36:55 -0700

    replace template

--- README.md
+++
@@ -1,4 +0,0 @@
-Copied using the example from:
-```
-https://github.com/Microsoft/TypeScript-Node-Starter.git 
-```
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,6 @@
-import errorHandler from "errorhandler";
+import express from 'express';
+const app = express();
 
-import app from "./app";
+app.get('/', (req, res) => res.send('Hello World!'));
 
-/**
- * Error Handler. Provides full stack - remove for production
- */
-app.use(errorHandler());
-
-/**
- * Start Express server.
- */
-const server = app.listen(app.get("port"), () => {
-  console.log(
-    "  App is running at http://localhost:%d in %s mode",
-    app.get("port"),
-    app.get("env")
-  );
-  console.log("  Press CTRL-C to stop\n");
-});
-
-export default server;
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
