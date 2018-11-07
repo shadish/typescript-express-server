b600572e9898e1f5a60b4eccd8731c1a1cb7f4db
shadish <shadtisch@gmail.com>
2018-11-07 00:10:55 -0700

    show cli output from endpoint

--- dist/server.js
+++ dist/server.js
@@ -5,7 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = express_1.default();
-app.get('/', (req, res) => res.send('Hello World!'));
+// import { exec } from 'child_process';
+const exec = require('child_process').exec;
+app.get('/', (req, res) => {
+    const child = exec('ls');
+    res.send(child);
+});
 const port = 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
+// const exec = require('child_process').exec;
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import express from 'express';
 const app = express();
+// import { exec } from 'child_process';
+const exec = require('child_process').exec;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+  const child = exec('ls');
+  res.send(child);
+});
 
-const port = 3000
+const port = 3000;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
+
+// const exec = require('child_process').exec;
