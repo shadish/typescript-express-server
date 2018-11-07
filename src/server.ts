81bd4ed7b192d21818c23fc0d1cce81e8117e39c
shadish <shadtisch@gmail.com>
2018-11-06 23:58:07 -0700

    abstract port

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,6 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+const port = 3000
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
