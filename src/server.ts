6e1823f2699e543932ade0f2dda548a0bc0fe624
shadish <shadtisch@gmail.com>
2018-11-14 23:32:04 -0700

    add wip express server

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,35 @@
 import express from 'express';
 const app = express();
-// import { exec } from 'child_process';
-const exec = require('child_process').exec;
+const exec = require("child_process").exec;
+
+const runCommand = cmd => {
+  return exec(`${cmd}`, (error, stdout, stderr) => {
+    // if (error) {
+    //   console.error(`exec error: ${error}`);
+    //   return;
+    // }
+    console.log(`stdout: ${stdout}`);
+    return `${stdout}`;
+
+    // console.log(`stderr: ${stderr}`);
+  });
+};
 
 app.get('/', (req, res) => {
-  const child = exec('ls');
-  res.send(child);
+  const ls = runCommand("ls");
+  res.send(ls);
+//   // const child = exec('ls');
+//   // res.send(child);
+
+// var spawn = require('child_process').spawn;
+// var x = spawn('env');
+// var ls  = spawn('ls', ['-l']);
+// ls.stdout.on('data', function (data) {
+//    console.log(data);
+// });
 });
 
-const port = 3000;
+// const port = 3000;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}`));
+// app.listen(port, () => console.log(`Example app listening on port ${port}`));
 
-// const exec = require('child_process').exec;
